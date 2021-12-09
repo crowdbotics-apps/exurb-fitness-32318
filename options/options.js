905dc4da59c8ada323fd6713d4a64821fdc486b6
Crowdbotics <team@crowdbotics.com>
2021-12-09 03:50:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "exurb_fitness_32318",
+  url: "https://exurb_fitness_32318.botics.co",
+  api: "https://exurb_fitness_32318.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
